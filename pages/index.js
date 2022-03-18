d45f62a1b50e2584f35c0be433a0a35855c6e142
Patrick Cash <patrickcash@gmail.com>
2022-03-18 18:53:34 -0400

    empty next.js project with tailwindcss configured

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Patrick Cash</title>
+        <meta name="description" content="Portfolio for Patrick Cash" />
+      </Head>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
