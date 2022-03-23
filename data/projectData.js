fd4dad41e752e7c980451ff616b33a7434ad77f8
Patrick Cash <patrickcash@gmail.com>
2022-03-23 16:48:40 -0400

    created pages and ocomponents for basic layout

---
+++ data/projectData.js
@@ -0,0 +1,39 @@
+const projectsData = [
+    {
+      title: '',
+      description: ``,
+      imgSrc: '',
+      github: '',
+      demo: ''
+    },
+    {
+        title: '',
+        description: ``,
+        imgSrc: '',
+        github: '',
+        demo: ''
+    },
+    {
+        title: '',
+        description: ``,
+        imgSrc: '',
+        github: '',
+        demo: ''
+    },
+    {
+        title: '',
+        description: ``,
+        imgSrc: '',
+        github: '',
+        demo: ''
+    },
+    {
+        title: '',
+        description: ``,
+        imgSrc: '',
+        github: '',
+        demo: ''
+    },
+  ]
+  
+  export default projectsData
---
+++ pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FourZeroFour() {
+  return (
+    <div>404</div>
+  )
+}
