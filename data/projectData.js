c67c58e59e35f24091f07e66bbddc9bcec02ba9e
Patrick Cash <patrickcash@gmail.com>
2022-03-31 22:12:42 -0400

    update project screenshot and fix typos

--- data/projectData.js
+++ data/projectData.js
@@ -1,8 +1,8 @@
 const projectsData = [
     {
         title: 'Really',
-        description: 'Demo realty site that allows users to view listings and contact the realtors',
-        techStack: 'React and Redux frontend with SASS styling. Django and Django Rest Framework backend.',
+        description: 'Demo realty site that allows users to view listings, search by criteria such as number of bedrooms, bathrooms, sqft, etc. and contact the realtors',
+        techStack: 'React and Redux frontend with Sass styling. Django and Django Rest Framework backend.',
         imgSrc: '/static/images/really.png',
         github: 'https://github.com/patrickcash/Really',
         demo: null
@@ -10,7 +10,7 @@ const projectsData = [
     {
         title: 'Research Wrangler',
         description: 'Researcher bookmark app that allows users to search for academic publications and bookmark them later',
-        techStack: 'React and Rredux frontend with Bootstrap styling. Django and Django Rest Framework backend.O',
+        techStack: 'React and Redux frontend with Bootstrap styling. Django and Django Rest Framework backend.',
         imgSrc: '/static/images/research_wrangler.png',
         github: 'https://github.com/patrickcash/research-wrangler',
         demo: null
@@ -42,7 +42,7 @@ const projectsData = [
     {
         title: 'Point Of Interest App',
         description: 'App allows users to store points of interest on a map by selecting the location and adding a title, review and rating',
-        techStack: 'MERN: MondoDB database, Express backend, REact frontend, running in a Node environment. Also uuses MapBox for map UI',
+        techStack: 'MERN: MondoDB database, Express backend, React frontend, running in a Node environment. Also uses MapBox for map UI',
         imgSrc: '/static/images/point_of_interest_app.png',
         github: 'https://github.com/patrickcash/point-of-interest-app',
         demo: 'https://point-of-interest-app.herokuapp.com/'
@@ -57,7 +57,7 @@ const projectsData = [
     },
     {
         title: 'React Dashboard',
-        description: 'Custom build demo dashboard',
+        description: 'Custom built demo dashboard',
         techStack: 'React app using custom styling and Chart.js',
         imgSrc: '/static/images/react_dashboard.png',
         github: 'https://github.com/patrickcash/react-dashboard',
@@ -65,8 +65,8 @@ const projectsData = [
     },
     {
         title: 'React Admin Dashboard',
-        description: 'Demo Dasboard built using REact Admin dashboard library',
-        techStack: 'Reaact app using React Admin library',
+        description: 'Demo Dashboard',
+        techStack: 'React dashboard app using React Admin library',
         imgSrc: '/static/images/react_admin_dashboard.png',
         github: 'https://github.com/patrickcash/react-admin-dashboard',
         demo: null
@@ -81,7 +81,7 @@ const projectsData = [
     },
     {
         title: 'Pixabay Photo Search',
-        description: 'Simple search UI for Pixabay photo service',
+        description: 'Simple search UI for Pixabay photo service API',
         techStack: 'React app with MaterialUI Styling',
         imgSrc: '/static/images/photo_search.png',
         github: 'https://github.com/patrickcash/photo-search',
@@ -89,7 +89,7 @@ const projectsData = [
     },
     {
         title: 'Full Screen Slide Show',
-        description: 'Simple slideshow that displays iamges full screen with descriptions',
+        description: 'Simple slideshow that displays images full screen with descriptions',
         techStack: 'Vanilla HTML, Javascript and CSS',
         imgSrc: '/static/images/full_screen_slide_show.png',
         github: 'https://github.com/patrickcash/fullscreen-image-slideshow',
