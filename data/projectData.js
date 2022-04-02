5261af768755ec4806b24bddd39895277ca22c7a
Patrick Cash <patrickcash@gmail.com>
2022-04-02 01:07:45 -0400

    added demo url and reordered projects list

--- data/projectData.js
+++ data/projectData.js
@@ -1,34 +1,26 @@
 const projectsData = [
     {
-        title: 'Really',
-        description: 'Demo realty site that allows users to view listings, search by criteria such as number of bedrooms, bathrooms, sqft, etc. and contact the realtors',
-        techStack: 'React and Redux frontend with Sass styling. Django and Django Rest Framework backend.',
-        imgSrc: '/static/images/really.png',
-        github: 'https://github.com/patrickcash/Really',
-        demo: null
-    },
-    {
-        title: 'Research Wrangler',
-        description: 'Researcher bookmark app that allows users to search for academic publications and bookmark them later',
-        techStack: 'React and Redux frontend with Bootstrap styling. Django and Django Rest Framework backend.',
-        imgSrc: '/static/images/research_wrangler.png',
-        github: 'https://github.com/patrickcash/research-wrangler',
-        demo: null
-    },
-    {
         title: 'Devcord',
         description: 'Discord inspired chat app',
         techStack: 'Django with custom CSS',
         imgSrc: '/static/images/devcord.png',
         github: 'https://github.com/patrickcash/devcord',
-        demo: null
+        demo: 'https://devcord-pc.herokuapp.com/'
     },
     {
-        title: 'Delicious Django',
-        description: 'del.icio.us inspired bookmark manager',
-        techStack: 'Django with Bootstrap styling',
-        imgSrc: '/static/images/delicious_django.png',
-        github: 'https://github.com/patrickcash/delicious-django',
+        title: 'Point Of Interest App',
+        description: 'App allows users to store points of interest on a map by selecting the location and adding a title, review and rating',
+        techStack: 'MERN: MondoDB database, Express backend, React frontend, running in a Node environment. Also uses MapBox for map UI',
+        imgSrc: '/static/images/point_of_interest_app.png',
+        github: 'https://github.com/patrickcash/point-of-interest-app',
+        demo: 'https://point-of-interest-app.herokuapp.com/'
+    },
+    {
+        title: 'Really',
+        description: 'Demo realty site that allows users to view listings, search by criteria such as number of bedrooms, bathrooms, sqft, etc. and contact the realtors',
+        techStack: 'React and Redux frontend with Sass styling. Django and Django Rest Framework backend.',
+        imgSrc: '/static/images/really.png',
+        github: 'https://github.com/patrickcash/Really',
         demo: null
     },
     {
@@ -40,12 +32,20 @@ const projectsData = [
         demo: 'https://nlp-library-comparison-app.herokuapp.com/'
     },
     {
-        title: 'Point Of Interest App',
-        description: 'App allows users to store points of interest on a map by selecting the location and adding a title, review and rating',
-        techStack: 'MERN: MondoDB database, Express backend, React frontend, running in a Node environment. Also uses MapBox for map UI',
-        imgSrc: '/static/images/point_of_interest_app.png',
-        github: 'https://github.com/patrickcash/point-of-interest-app',
-        demo: 'https://point-of-interest-app.herokuapp.com/'
+        title: 'Research Wrangler',
+        description: 'Researcher bookmark app that allows users to search for academic publications and bookmark them later',
+        techStack: 'React and Redux frontend with Bootstrap styling. Django and Django Rest Framework backend.',
+        imgSrc: '/static/images/research_wrangler.png',
+        github: 'https://github.com/patrickcash/research-wrangler',
+        demo: null
+    },
+    {
+        title: 'Delicious Django',
+        description: 'del.icio.us inspired bookmark manager',
+        techStack: 'Django with Bootstrap styling',
+        imgSrc: '/static/images/delicious_django.png',
+        github: 'https://github.com/patrickcash/delicious-django',
+        demo: null
     },
     {
         title: 'Reader',
