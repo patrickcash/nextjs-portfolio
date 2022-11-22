bff043f4eb003aedb34a02e93e8a3e9147b5f26b
Patrick Cash <patrickcash@gmail.com>
2022-11-22 14:23:11 -0500

    remove heroku demo apps

--- data/projectData.js
+++ data/projectData.js
@@ -1,29 +1,5 @@
 const projectsData = [
     {
-        title: 'Point Of Interest App',
-        description: 'App allows users to store points of interest on a map by selecting the location and adding a title, review and rating',
-        techStack: 'Full Stack MERN: MongoDB database, Express backend, React frontend, running in a Node environment. Also uses MapBox for map UI',
-        imgSrc: '/static/images/point_of_interest_app.png',
-        github: 'https://github.com/patrickcash/point-of-interest-app',
-        demo: 'https://point-of-interest-app.herokuapp.com/'
-    },
-    {
-        title: 'Really',
-        description: 'Demo realty site that allows users to view listings, search by criteria such as number of bedrooms, bathrooms, sqft, etc. and contact the realtors',
-        techStack: 'Full Stack: React and Redux frontend with Sass styling. Django and Django Rest Framework backend with PostgreSQL database.',
-        imgSrc: '/static/images/really.png',
-        github: 'https://github.com/patrickcash/Really',
-        demo: 'https://really-pc.herokuapp.com/'
-    },
-    {
-        title: 'Devcord',
-        description: 'Discord inspired chat app',
-        techStack: 'Full Stack: Django with custom CSS and PostgreSQL database',
-        imgSrc: '/static/images/devcord.png',
-        github: 'https://github.com/patrickcash/devcord',
-        demo: 'https://devcord-pc.herokuapp.com/'
-    },
-    {
         title: 'Tailwind Photo Search',
         description: 'Simple search UI for Pixabay photo service',
         techStack: 'Front End: React app with TailwindCSS styling',
@@ -40,6 +16,38 @@ const projectsData = [
         demo: 'https://react-dashboard-pc.netlify.app'
     },
     {
+        title: 'Pixabay Photo Search',
+        description: 'Simple search UI for Pixabay photo service API',
+        techStack: 'Front End: React app with MaterialUI Styling',
+        imgSrc: '/static/images/photo_search.png',
+        github: 'https://github.com/patrickcash/photo-search',
+        demo: 'https://pixabay-photo-search.netlify.app'
+    },
+    {
+        title: 'Point Of Interest App',
+        description: 'App allows users to store points of interest on a map by selecting the location and adding a title, review and rating',
+        techStack: 'Full Stack MERN: MongoDB database, Express backend, React frontend, running in a Node environment. Also uses MapBox for map UI',
+        imgSrc: '/static/images/point_of_interest_app.png',
+        github: 'https://github.com/patrickcash/point-of-interest-app',
+        demo: null
+    },
+    {
+        title: 'Really',
+        description: 'Demo realty site that allows users to view listings, search by criteria such as number of bedrooms, bathrooms, sqft, etc. and contact the realtors',
+        techStack: 'Full Stack: React and Redux frontend with Sass styling. Django and Django Rest Framework backend with PostgreSQL database.',
+        imgSrc: '/static/images/really.png',
+        github: 'https://github.com/patrickcash/Really',
+        demo: null
+    },
+    {
+        title: 'Devcord',
+        description: 'Discord inspired chat app',
+        techStack: 'Full Stack: Django with custom CSS and PostgreSQL database',
+        imgSrc: '/static/images/devcord.png',
+        github: 'https://github.com/patrickcash/devcord',
+        demo: null
+    },
+    {
         title: 'Chatbot',
         description: 'Chatbot using the chatterbot library and their predefined corpus',
         techStack: 'Full Stack: React frontend with the React Chatbot Kit library. Flask backend with the Chatterbot NLP library',
@@ -53,7 +61,7 @@ const projectsData = [
         techStack: 'Full Stack: Flask app with Bootstap styling. Uses TextBlob(NLTK), SpaCy, VADER and Gensim python NLP libraries',
         imgSrc: '/static/images/nlp_library_comparison_app.png',
         github: 'https://github.com/patrickcash/nlp-library-comparison-app',
-        demo: 'https://nlp-library-comparison-app.herokuapp.com/'
+        demo: null
     },
     {
         title: 'Research Wrangler',
@@ -78,14 +86,6 @@ const projectsData = [
         imgSrc: '/static/images/reader.png',
         github: 'https://github.com/patrickcash/Reader',
         demo: null
-    },
-    {
-        title: 'Pixabay Photo Search',
-        description: 'Simple search UI for Pixabay photo service API',
-        techStack: 'Front End: React app with MaterialUI Styling',
-        imgSrc: '/static/images/photo_search.png',
-        github: 'https://github.com/patrickcash/photo-search',
-        demo: 'https://pixabay-photo-search.netlify.app'
     },
     {
         title: 'Full Screen Slide Show',
